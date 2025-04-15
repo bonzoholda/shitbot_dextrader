6d16f93d9b7cf5324f932127751a0336f177273c
bonzoholda <bhumibeka@gmail.com>
2025-04-15 22:40:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def start_bot():
             bot.trading_execution()
         except Exception as e:
             logging.error(f"Error in bot: {e}")
-        time.sleep(15)
+        time.sleep(20)
 
 threading.Thread(target=start_bot, daemon=True).start()
 
